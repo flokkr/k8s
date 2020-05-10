9fa828ed1b54cbcd81cbc4e8cf79e5951636062f
Elek Márton <elek@apache.org>
2020-05-10 13:01:23 +0200

    fix generation of keystore

--- ca/output/getkeystore.sh
+++ ca/output/getkeystore.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 STATEDIR=".state/keystore"
 mkdir -p "$STATEDIR"
 CADIR=".state/ca"
 DESCRIPTOR="$1"
+NAME=$(basename $1)
 STATEFILE="$STATEDIR/$NAME"
 if [ ! -f "$STATEFILE" ]; then
    source "$DESCRIPTOR"
-   certstrap --depot-path "$CADIR" request-cert --passphrase "" --domain $PRIMARY_DOMAIN,$OTHER_DOMAINS 1>&2
-   certstrap --depot-path "$CADIR" sign --CA ca $PRIMARY_DOMAIN 1>&2
+   if [ ! -f "$CADIR/$PRIMARY_DOMAIN.key" ]; then
+     certstrap --depot-path "$CADIR" request-cert --passphrase "" --domain $PRIMARY_DOMAIN,$OTHER_DOMAINS 1>&2
+     certstrap --depot-path "$CADIR" sign --CA ca $PRIMARY_DOMAIN 1>&2
+   fi
    openssl pkcs12 -export \
        -in $CADIR/$PRIMARY_DOMAIN.crt \
        -inkey $CADIR/$PRIMARY_DOMAIN.key \
